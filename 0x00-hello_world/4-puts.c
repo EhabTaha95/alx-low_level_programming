762198e3d0de18ef034b7944a66cec9098e3df93
Ehab Taha <ehabtaha4800@gmail.com>
2023-02-20 13:40:11 +0200

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 {
-	char Mystring = "Programming is like building a multilingual puzzle";
-	puts(Mystring);
-	return(0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
