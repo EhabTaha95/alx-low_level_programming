1bb9533ab35583765f9c401c28517c2556492f0e
Ehab Taha <ehabtaha4800@gmail.com>
2023-02-20 14:33:06 +0200

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - simple main function to print msg to stdo
+ * Return 1 exit code
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
