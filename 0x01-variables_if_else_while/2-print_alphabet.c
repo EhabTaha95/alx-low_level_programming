ee2571dfbacd9256c6ec95c2e8236bfeb3c7d292
Ehab Taha <ehabtaha4800@gmail.com>
2023-02-20 21:36:12 +0200

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function to write all alpha
+ *
+ * Return: 0 exit code
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		putchar(alph);
+	return (0);
+}
