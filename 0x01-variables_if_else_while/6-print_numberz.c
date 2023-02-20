739665f131dd1d08085b33fae732b31b9fd7e32f
Ehab Taha <ehabtaha4800@gmail.com>
2023-02-20 22:46:43 +0200

    7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - main function to write numbers till 10
+ *
+ *Return: 0 exit code
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - main function to write all alpha
+ *
+ *Return: 0 exit code
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'z'; alph >= 'a'; alph--)
+		putchar(alph);
+
+		putchar('\n');
+		return (0);
+}
