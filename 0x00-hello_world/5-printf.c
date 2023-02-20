3eb61999174b1186ebb17444fd7eb7f033c417ac
Ehab Taha <ehabtaha4800@gmail.com>
2023-02-20 13:54:37 +0200

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - simple main function print msg to standard output using printf
+ * retun: 0 exit code
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
