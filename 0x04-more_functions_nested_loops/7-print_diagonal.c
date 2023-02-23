3e27879ec7b3df08563d60055840be961736e6c0
Ehab Taha <ehabtaha4800@gmail.com>
2023-02-23 15:15:10 +0200

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+# include "main.h"
+
+/**
+ * print_diagonal - function that prints a diagonal line
+ * @n: character to be checked
+ * Return: diagonal line
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else if (n == 1)
+	{
+		_putchar(\);
+				_putchar('\n');
+	}
+	else
+	{
+		int j, m;
+
+		for (m = 1; m < n; m++)
+		{
+		for (j = 1; j <= m; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(\);
+		_putchar('\n');
+		}
+	}
+}
+
