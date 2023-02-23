cf75e1e9a9fdad89abd8a55dc0c5e0fe7958736b
Ehab Taha <ehabtaha4800@gmail.com>
2023-02-23 13:39:57 +0200

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+# include "main.h"
+
+/**
+ * mul - function that multiply two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result of multiplying a*b
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+}
