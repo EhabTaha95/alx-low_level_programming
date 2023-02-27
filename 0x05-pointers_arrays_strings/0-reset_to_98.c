4cad18ac92de60bcf402eb3b62f49d3c2b067baa
Ehab Taha <ehabtaha4800@gmail.com>
2023-02-27 21:57:30 +0200

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+ * reset_to_98 - funtion that takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ * @n: ponter for int
+ * Return: value of 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+
+}
