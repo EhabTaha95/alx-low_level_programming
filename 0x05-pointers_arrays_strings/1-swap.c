d7a3864192bdd70bf3df4ad88703653911893a26
Ehab Taha <ehabtaha4800@gmail.com>
2023-02-27 22:09:17 +0200

    2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+# include "main.h"
+
+/**
+ * swap_int - swap the values of @a and @b
+ * @a: the reference to 1st variable
+ * @b: the reference to 2nd variable
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
